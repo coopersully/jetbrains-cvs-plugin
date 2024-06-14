4458a0159360097266752d1bb53aa42c5d39c029
GitHub Action <action@github.com>
2024-06-14 19:23:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/coopersully/jetbrainscvsplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/coopersully/jetbrainscvsplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.coopersully.jetbrainscvsplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
